f45bd1b111ab058bbb8afe35b6b95211a605b1bc
Roy <rxlisbest@gmail.com>
2019-08-07 15:35:33 +0800

    修改任务

--- application/http/Worker.php
+++ application/http/Worker.php
@@ -19,7 +19,7 @@ class Worker extends Server
 
     public function onMessage($connection, $data)
     {
-        if ($data == 1) {
+        if (true) {
             Message::send(1, function ($data) use ($connection) {
                 $result = $connection->send($data);
                 if ($result == null) {
