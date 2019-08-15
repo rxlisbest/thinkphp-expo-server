1117e529c88a797ccb2f045b8ba8d3e15b6691a3
Roy <rxlisbest@gmail.com>
2019-08-15 15:49:05 +0800

    测试下拉

--- public/ipad-source-code/src/App.vue
+++ public/ipad-source-code/src/App.vue
@@ -7,11 +7,41 @@
 <script>
   export default {
     name: 'app',
-    components: {}
+    components: {},
+    create() {
+      var overscroll = function(el) {
+        el.addEventListener('touchstart', function() {
+          var top = el.scrollTop
+            ,totalScroll = el.scrollHeight
+            ,currentScroll = top + el.offsetHeight;
+          if(top === 0) {
+            el.scrollTop = 1;
+          }else if(currentScroll === totalScroll) {
+            el.scrollTop = top - 1;
+          }
+        });
+
+        el.addEventListener('touchmove', function(evt) {
+          if(el.offsetHeight < el.scrollHeight)
+            evt._isScroller = true;
+        });
+      }
+
+      overscroll(document.querySelector('.scroll'));
+      document.body.addEventListener('touchmove', function(evt) {
+        if(!evt._isScroller) {
+          evt.preventDefault();
+        }
+      });
+    }
   }
 </script>
 
 <style>
+  #app{
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
   #app {
     width: 100%;
     height: 100%;
